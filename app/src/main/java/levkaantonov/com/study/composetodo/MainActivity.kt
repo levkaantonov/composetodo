8354216450f3d694b293037bb31acf865a274450
levkaantonov <levkaantonov@gmail.com>
2021-12-07 13:03:07 +0300

    finish

--- app/src/main/java/levkaantonov/com/study/composetodo/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetodo/MainActivity.kt
@@ -3,22 +3,36 @@ package levkaantonov.com.study.composetodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import levkaantonov.com.study.composetodo.ui.theme.ComposetodoTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<TodoViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposetodoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    TodoActivityScreen(todoViewModel = viewModel)
                 }
             }
         }
+    }
+
+    @Composable
+    private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+        TodoScreen(
+            items = todoViewModel.todoItems,
+            currentlyEditing = todoViewModel.currentEditItem,
+            onAddItem = todoViewModel::addItem,
+            onRemoveItem = todoViewModel::removeItem,
+            onStartEdit = todoViewModel::onEditItemSelected,
+            onEditItemChange = todoViewModel::onEditItemChange,
+            onEditDone = todoViewModel::onEditDone
+        )
     }
 }
--- app/src/main/java/levkaantonov/com/study/composetodo/TodoComponents.kt
+++ app/src/main/java/levkaantonov/com/study/composetodo/TodoComponents.kt
@@ -1,4 +1,176 @@
 package levkaantonov.com.study.composetodo
 
-class TodoComponents {
+import androidx.annotation.StringRes
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AnimatedIconRow(
+    icon: TodoIcon,
+    onIconChange: (TodoIcon) -> Unit,
+    modifier: Modifier = Modifier,
+    visible: Boolean = true,
+) {
+    val enter = remember { fadeIn(animationSpec = TweenSpec(300, easing = FastOutLinearInEasing)) }
+    val exit = remember { fadeOut(animationSpec = TweenSpec(100, easing = FastOutSlowInEasing)) }
+    Box(modifier.defaultMinSize(minHeight = 16.dp)) {
+        AnimatedVisibility(
+            visible = visible,
+            enter = enter,
+            exit = exit,
+        ) {
+            IconRow(icon, onIconChange)
+        }
+    }
 }
+
+@Composable
+fun IconRow(
+    icon: TodoIcon,
+    onIconChange: (TodoIcon) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier) {
+        for (todoIcon in TodoIcon.values()) {
+            SelectableIconButton(
+                icon = todoIcon.imageVector,
+                iconContentDescription = todoIcon.contentDescription,
+                onIconSelected = { onIconChange(todoIcon) },
+                isSelected = todoIcon == icon
+            )
+        }
+    }
+}
+
+@Composable
+private fun SelectableIconButton(
+    icon: ImageVector,
+    @StringRes iconContentDescription: Int,
+    onIconSelected: () -> Unit,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val tint = if (isSelected) {
+        MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+    }
+    TextButton(
+        onClick = { onIconSelected() },
+        shape = CircleShape,
+        modifier = modifier
+    ) {
+        Column {
+            Icon(
+                imageVector = icon,
+                tint = tint,
+                contentDescription = stringResource(id = iconContentDescription)
+            )
+            if (isSelected) {
+                Box(
+                    Modifier
+                        .padding(top = 3.dp)
+                        .width(icon.defaultWidth)
+                        .height(1.dp)
+                        .background(tint)
+                )
+            } else {
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoItemInputBackground(
+    elevate: Boolean,
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    val animatedElevation by animateDpAsState(if (elevate) 1.dp else 0.dp, TweenSpec(500))
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.05f),
+        elevation = animatedElevation,
+        shape = RectangleShape,
+    ) {
+        Row(
+            modifier = modifier.animateContentSize(animationSpec = TweenSpec(300)),
+            content = content
+        )
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun TodoInputText(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    onImeAction: () -> Unit = {}
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    TextField(
+        value = text,
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyboardController?.hide()
+        }),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TodoEditButton(
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    TextButton(
+        onClick = onClick,
+        shape = CircleShape,
+        enabled = enabled,
+        modifier = modifier
+    ) {
+        Text(text)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewIconRow() = IconRow(icon = TodoIcon.Square, onIconChange = {})
+
+@Preview
+@Composable
+fun AnimatedIconRow() = AnimatedIconRow(icon = TodoIcon.Square, onIconChange = {})
+
+@Preview
+@Composable
+fun PreviewTodoEditButton() = TodoEditButton(onClick = { }, text = "21d3" )
